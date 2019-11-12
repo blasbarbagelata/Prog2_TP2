8e04d664188b07e9af55594fc22bbec387e5b1a4
FranKKastor <FranKKastor@users.noreply.github.com>
2019-11-12 19:17:11 -0300

    maldito bill gates

--- main.c
+++ main.c
@@ -20,27 +20,28 @@ int CantidadDeLineas(FILE *Archivo){
     return Lineas;
 }
 int FuncionSubPrincipal(int N,int NumPersonas){
-    long Array_Index[N], NumeroRandom, Array_Random[NumPersonas];
-    for(long i=0;i<NumPersonas;++i){
+    int Array_Index[N], NumeroRandom, Array_Random[NumPersonas];
+    for(int i=0;i<NumPersonas;++i){
         Array_Random[i]=0; // Inicializa
     }
     srand((unsigned int)time(NULL));
-    printf("%d\n",(NumPersonas/RAND_MAX));
+    printf("%d\n",RAND_MAX);
     for(int i=N; i!=0;--i){
-        NumeroRandom = (NumPersonas/RAND_MAX)*(rand() % (NumPersonas+1)) + rand()%(NumPersonas-(NumPersonas%RAND_MAX)+1);
-        while(Array_Random[NumeroRandom]==0){
-            Array_Random[NumeroRandom]=-1;
+        NumeroRandom = rand() % (NumPersonas+1);
+        while(Array_Random[NumeroRandom]!=0){
+            NumeroRandom = rand() % (NumPersonas+1);
         }
+        Array_Random[NumeroRandom]=-1;
     }
     int n=0;
-    for(long i=0;i<NumPersonas;++i){
+    for(int i=0;i<NumPersonas;++i){
         if(Array_Random[i]==-1){
             Array_Index[n]=i;
             n++;
         }
     }
     for(int i=0;i<N;++i){
-        printf("%ld\n",Array_Index[i]);
+        printf("%d\n",Array_Index[i]);
     }
     return 0;
 }
