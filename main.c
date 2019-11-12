177554e1d971cf75d2556444809eb8385e40f993
FranKKastor <FranKKastor@users.noreply.github.com>
2019-11-12 11:27:49 -0300

    la pifie

--- main.c
+++ main.c
@@ -1,26 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-int Pertenece (char palabra[],char letra){
-    int contador=0;
-    for(int i=0; palabra[i]!= '\0';++i){
-        if (palabra[i]==letra){
-                contador=1;
+#include <time.h>
+typedef struct{
+    char Nombre[];
+    char Localidad[30];
+    int Genero;
+    int Interes;
+} PersonaEntrada;
+int CantidadDeLineas(FILE *Archivo){
+    long Lineas = 0;
+    char caracter;
+    caracter= fgetc(Archivo);
+    while(caracter!=EOF){
+        if(caracter =='\n'){
+            Lineas++;
         }
+        caracter= fgetc(Archivo);
     }
-    return contador;
+    return Lineas;
 }
-int main()
-{
-    int bandera;
-    char palabra[25];
-    char Caracter='a';
-    printf("Ingrese una palabra: ");
-    scanf("%s",palabra);
-    printf("ingrese un caracter: ");
-    scanf("%c",&Caracter);
-    bandera = Pertenece(palabra,Caracter);
-    printf("%d\n",Pertenece(palabra,Caracter));
-    printf("%d\n",bandera);
+int FuncionSubPrincipal(int N,int NumPersonas){
+    int NumeroR, Index[NumPersonas]; //Arreglo de numeros random
+    for(int i=0;i<NumPersonas;++i){
+        Index[i]=0; // Inicializa
+    }
+    for(int i=N; i!=0;--i){
+        NumeroR = rand() % NumPersonas;
+        if(N[NumeroR]!=-1)
+    }
+}
+int main (){
+    int Cantidad=0,Lineas=0;
+    FILE *Entrada;
+    printf("Ingrese la cantidad de personas: ");
+    scanf("%d",&Cantidad);
+    Entrada =fopen("personas.txt","r");
+    Lineas = CantidadDeLineas(Entrada);
+    fclose(Entrada);
+    while (Cantidad>Lineas){
+        printf("Por favor ingrese una cantidad menor a %d: ",Lineas);
+        scanf("%d",&Cantidad);
+    }
+    FuncionSubPrincipal(Cantidad,Lineas);
+
     return 0;
 }
