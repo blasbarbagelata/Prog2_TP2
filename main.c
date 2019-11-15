3b08aa47dcc55a0e135dd8a73f57be565a6352da
chaloxx <pabloalonsoxx@gmail.com>
2019-11-15 12:22:29 -0300

    solucionado el problema de escribir uno de mas y \n fantasma, hay que solucionar normalizar las localidades

--- main.c
+++ main.c
@@ -12,7 +12,7 @@ int CantidadDeLineas(FILE *Archivo){
         }
         caracter= fgetc(Archivo);
     }
-    return Lineas+1;
+    return Lineas;
 }
 void Normalizar(char array[]){
     int i=0;
@@ -25,38 +25,34 @@ void CrearArregloLocalidades(FILE *Archivo,int Tamano,char *Localidades[]){
     char basura[50], buffer[100];
     for (int i=0; i < Tamano;++i){
         fscanf(Archivo,"%[^,],",basura);
-        fscanf(Archivo,"%[^\n]\n",buffer);
+        fscanf(Archivo,"%[^\n]",buffer);
         Normalizar(buffer);
         Localidades[i] = malloc(sizeof(char)*60);
         strcpy(Localidades[i], buffer);
     }
 }
 void funcionlectura(int CantPersonas, int arrayRandom[]){
-    FILE *Archibopersonas,*archibolocalidades,*ArchivoSalida;
+    FILE *Archivopersonas,*Archivolocalidades,*ArchivoSalida;
     char buffer[100],Genero[2]={'M','F'},Interes[4]={'F','M','A','N'};
     char Nombre[50],Apellido[50];
-    int linea=1,Codigo,Sexo,Sexualidad,Edad,tamano;
-    archibolocalidades =fopen("codigoLocalidades.txt","r");
-    tamano = CantidadDeLineas(archibolocalidades);
+    int Linea=0,Codigo,Sexo,Sexualidad,Edad,tamano;
+    Archivolocalidades =fopen("codigoLocalidades.txt","r");
+    tamano = CantidadDeLineas(Archivolocalidades);
     char *ArregloLocalidades[tamano];
-    rewind(archibolocalidades);
-    CrearArregloLocalidades(archibolocalidades,tamano,ArregloLocalidades);
-    fclose(archibolocalidades);
-    Archibopersonas =fopen("personas.txt","r");
+    rewind(Archivolocalidades);
+    CrearArregloLocalidades(Archivolocalidades,tamano,ArregloLocalidades);
+    fclose(Archivolocalidades);
+    Archivopersonas =fopen("personas.txt","r");
     ArchivoSalida =fopen("ArchivoSalida.txt","w");
     for(int i=0; i< CantPersonas; i++){
-        while(linea<arrayRandom[i]){
-            fgets(buffer,100,Archibopersonas);
-            linea++;
+        while(Linea<arrayRandom[i]){
+            fgets(buffer,100,Archivopersonas);
+            Linea++;
         }
-        fscanf(Archibopersonas,"%[^,],%[^,],",Nombre,Apellido);
-        fscanf(Archibopersonas,"%d,",&Codigo);
-        fscanf(Archibopersonas,"%d,",&Edad);
-        fscanf(Archibopersonas,"%d,",&Sexo);
-        fscanf(Archibopersonas,"%d",&Sexualidad);
+        fscanf(Archivopersonas,"%[^,],%[^,],%d,%d,%d,%d",Nombre,Apellido,&Codigo,&Edad,&Sexo,&Sexualidad);
         fprintf(ArchivoSalida,"%s, %s, %s, %d, %c, %c\n",Nombre,Apellido,ArregloLocalidades[Codigo-1],Edad,Genero[Sexo-1],Interes[Sexualidad-1]);
     }
-    fclose(Archibopersonas);
+    fclose(Archivopersonas);
     fclose(ArchivoSalida);
 }
 void FuncionSubPrincipal(int CantPersonas,int TotalPersonas){
@@ -68,9 +64,9 @@ void FuncionSubPrincipal(int CantPersonas,int TotalPersonas){
     }
     srand((unsigned int)time(NULL));
     for(int i=CantPersonas; i!=0;--i){
-        NumeroRandom = rand() % (TotalPersonas+1);
+        NumeroRandom = rand() % (TotalPersonas);
         while(Array_Random[NumeroRandom]!=0){
-            NumeroRandom = rand() % (TotalPersonas+1);
+            NumeroRandom = rand() % (TotalPersonas);
         }
         Array_Random[NumeroRandom]=-1;
     }
@@ -86,12 +82,12 @@ void FuncionSubPrincipal(int CantPersonas,int TotalPersonas){
 int main (){
     int Cantidad=0,Lineas=0;
     FILE *Entrada;
-    printf("Ingrese la cantidad de personas: ");
-    scanf("%d",&Cantidad);
     Entrada =fopen("personas.txt","r");
     Lineas = CantidadDeLineas(Entrada);
     fclose(Entrada);
-    while ((Cantidad>Lineas)&&(Cantidad<0)){
+    printf("Ingrese la cantidad de personas: ");
+    scanf("%d",&Cantidad);
+    while ((Cantidad>Lineas)||(Cantidad<0)){
         printf("Por favor ingrese una cantidad menor a %d y mayor a 0: ",Lineas);
         scanf("%d",&Cantidad);
     }
